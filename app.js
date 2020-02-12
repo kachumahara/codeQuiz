70de075b2788213a97d2927f8a4ec0e6522ed0d4
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-02-12 09:04:14 -0700

    added some functions

--- app.js
+++ app.js
@@ -38,7 +38,6 @@ var questions = [
 	{
 		question: 'A very useful tool used during development and debugging for printing content to the debugger is?',
 		answers: [
-			
 			{ text: 'Javascript', correct: false },
 			{ text: 'Terminal/bash', correct: false },
 			{ text: 'For loops', correct: false },
@@ -46,70 +45,67 @@ var questions = [
 		],
 	},
 ];
-var timeEl = document.querySelector(".time");
-var mainEl = document.getElementById("main");
+var timeEl = document.querySelector('.time');
+var mainEl = document.getElementById('main');
 var startButton = document.getElementById('start');
 var questionContainer = document.getElementById('question-box');
-var answerContainer = document.getElementById("answer-box")
-var instruction = document.getElementById("instruction-text")
-var questionIndex = 0
-var time = 60
-var timeInterval 
+var answerContainer = document.getElementById('answer-box');
+var instruction = document.getElementById('instruction-text');
+var questionIndex = 0;
+var time = 60;
+var timeInterval;
 
-startButton.addEventListener("click", function(){
-	//hide instruction 
-	instruction.setAttribute("class", "hide");
+startButton.addEventListener('click', function() {
+	//hide instruction
+	instruction.setAttribute('class', 'hide');
 
-	//show the first question
-	displayQuestion();
-
+	
 	// start the timer
 
 	// answer question and move to next question
-
-
-})
-
+});
 
 //function to display the Question and answer choices
-function displayQuestion () {
-questionContainer.textContent = questions[questionIndex].question
-answerContainer.children[0].textContent = questions[questionIndex].answers[0].text
-answerContainer.children[2].textContent = questions[questionIndex].answers[2].text
-answerContainer.children[3].textContent = questions[questionIndex].answers[3].text
-answerContainer.children[4].textContent = questions[questionIndex].answers[4].text
+function displayQuestion() {
+	questionContainer.textContent = questions[questionIndex].question;
+	questions[questionIndex].answers.forEach(function(answer, i){
+		answerContainer.children[i].textContent = answer.text;
+		answerContainer.children[i].setAttribute("data-index", i);
+	})
+	// answerContainer.children[0].textContent = questions[questionIndex].answers[0].text;
+	// answerContainer.children[1].textContent = questions[questionIndex].answers[1].text;
+	// answerContainer.children[2].textContent = questions[questionIndex].answers[2].text;
+	// answerContainer.children[3].textContent = questions[questionIndex].answers[3].text;
 	//console.log(questionContainer.children)
 
-//display question with question container query selector
+	//display question with question container query selector
 
-
-
-
-//loop through your answer choices with a for loop
-//and append those to your answer box
-
-
+	//loop through your answer choices with a for loop
+	//and append those to your answer box
+	// var text = '';
+	// var i;
+	// for (i = 0; i < question.length; i++) {}
 }
 
 //function that checks the user answer choice
-function checkAnswer () {
-
-}
+function checkAnswer() {}
 
 //event listener to listen for when answer is submitted
-
-
 
 // function StartGame() {};
 function setTime() {
 	var timerInterval = setInterval(function() {
-	  secondsLeft--;
-	  timeEl.textContent = secondsLeft + " Coding Quiz Challenge.";
-  
-	  if(secondsLeft === 0) {
-		clearInterval(timerInterval);
-		sendMessage();
-	  }
-  
+		secondsLeft--;
+		timeEl.textContent = secondsLeft + ' Coding Quiz Challenge.';
+
+		if (secondsLeft === 0) {
+			clearInterval(timerInterval);
+			sendMessage();
+		}
 	}, 1000);
-  }
+}
+
+answerContainer.addEventListener("click", function(e){
+	if(!e.target.matches("button")) return;
+	console.log(e.target);
+});
